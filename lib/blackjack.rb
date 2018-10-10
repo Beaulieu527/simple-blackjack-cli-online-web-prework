556cc54081b56d7e56b3629efe85b0f3e7113915
beaulieudev <beaulieudev@gmail.com>
2018-10-10 02:12:11 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,9 +1,5 @@
-##################
-# shared methods #
-##################
-
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
@@ -11,68 +7,54 @@ def deal_card
 end
 
 def display_card_total(card_total)
-  puts "Your cards add up to #{card_total}"
+  puts "Your cards add up to #{card_total}".
 end
-
-
-##########
-# rounds #
-##########
-
-def welcome
-  puts "Welcome to the Blackjack Table"
-end
-
 
 def prompt_user
   puts "Type 'h' to hit or 's' to stay"
 end
 
+def get_user_input
+  gets.chomp
+end
+
+def end_game
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
+end
 
 def initial_round
-  first_round = deal_card + deal_card
-  display_card_total(first_round)
-  return first_round
-end
-
-def get_user_input
-  gets.chomp.strip
-end
-
-def end_game(card_total)
-  puts "Sorry, you hit #{card_total}. Thanks for playing!"
+  card_total = deal_card + deal_card
+  display_card_total(card_total)
 end
 
 def hit?(card_total)
   prompt_user
   input = get_user_input
-  until input == 'h' || input == 's'
-    invalid_command
-    prompt_user
-    input = get_user_input
-  end
-  if input == 'h'
-    card_total += deal_card
-  elsif input == 's'
+  if input == "s"
     card_total
+  elsif input == "h"
+    card_total += deal_card
+  else
+   invalid_command
+   prompt_user
+   get_user_input
   end
 end
-
 
 def invalid_command
   puts "Please enter a valid command"
 end
 
-##########
-# runner #
-##########
+#####################################################
+# get every test to pass before coding runner below #
+#####################################################
 
 def runner
   welcome
   card_total = initial_round
-  until card_total > 21
-    card_total = hit?(card_total)
-    display_card_total(card_total)
+  until card_total >= 21
+   card_total = hit?(card_total)
+   display_card_total(card_total)
   end
   end_game(card_total)
 end
